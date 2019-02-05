c1e677611469edabff3e8b971a9856b0d7685908
Dave Ansell <dave@sciansell.co.uk>
2019-02-05 19:09:15 +0000

    make buttons work

--- node_helper.js
+++ node_helper.js
@@ -25,15 +25,26 @@ module.exports = NodeHelper.create({
 
       if (DEBUG) console.log("LEFT: " + leftDist + ", RIGHT: " + rightDist);
 
-      if (leftDist <= threshold && rightDist <= threshold) {
-        if (leftDist * distDiff <= rightDist) {
+      	if (leftDist <= threshold && rightDist >= threshold) {
+	  console.log(" left button pressed");
           this.sendSocketNotification("PAGE_INCREMENT", null);
           this.sendSocketNotification("PAGE_ACTIVATE", null);
-        } else if (rightDist * distDiff <= leftDist) {
-          this.sendSocketNotification("PAGE_DECREMENT", null);
-          this.sendSocketNotification("PAGE_ACTIVATE", null);
+	}else if (rightDist <= threshold && leftDist >= threshold) {
+	  console.log(" right button pressed");
+	  this.sendSocketNotification("PAGE_DECREMENT", null);
+	  this.sendSocketNotification("PAGE_ACTIVATE", null);
+	}else if (leftDist <= threshold && rightDist <= threshold) {
+	    if(leftDist * distDiff <= rightDist) {
+	  	console.log(" left button pressed2");
+          	this.sendSocketNotification("PAGE_INCREMENT", null);
+          	this.sendSocketNotification("PAGE_ACTIVATE", null);
+            } else if (rightDist * distDiff <= leftDist) {
+	  	console.log(" right button pressed2");
+          	this.sendSocketNotification("PAGE_DECREMENT", null);
+          	this.sendSocketNotification("PAGE_ACTIVATE", null);
+	    }
         }
-      }
+      
     });
 
     child.stdout.on("exit", function(exitCode) {
